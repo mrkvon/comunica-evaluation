b51fa888787d135135d9a6363d14ec8180362d39
mrkvon <mrkvon@protonmail.com>
2022-04-10 17:50:32 +0200

    Downgrade react-scripts, fix issues, install and try out @comunica
    React scripts downgraded because of issues with new webpack 5
    and polyfill of node libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { QueryEngine } from '@comunica/query-sparql-link-traversal-solid'
+
+const myEngine = new QueryEngine()
 
 function App() {
+  useEffect(() => {
+    (async () => {
+
+      const bindingsStream = await myEngine.queryBindings(`
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX mypod: <https://mrkvon.solidcommunity.net/profile/card#>
+        SELECT ?friend ?name
+        WHERE {
+          mypod:me foaf:knows ?friend.
+          ?friend foaf:name ?name.
+        }`, {
+          sources: ['https://mrkvon.solidcommunity.net/profile/card'],
+          lenient: true,
+      });
+      
+      // Consume results as a stream (best performance)
+      bindingsStream.on('data', (binding) => {
+        console.log('************************')
+          console.log(binding.toString()); // Quick way to print bindings for testing
+      });
+      bindingsStream.on('end', () => {
+        console.log('finished .........................')
+          // The data-listener will not be called anymore once we get here.
+      });
+      bindingsStream.on('error', (error) => {
+          console.error(error);
+      });
+    })()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
